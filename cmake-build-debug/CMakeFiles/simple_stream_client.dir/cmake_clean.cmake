48c10b3804a5e8853b3dc6d732e986f03cfc1c81
Brooke Weborg <brookeweborg@yahoo.com>
2017-11-03 14:29:35 -0400

    Made c++ Project

---
+++ cmake-build-debug/CMakeFiles/simple_stream_client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_stream_client.dir/main.cpp.o"
+  "simple_stream_client.pdb"
+  "simple_stream_client"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_stream_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
